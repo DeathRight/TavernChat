b3a324373b61f7c146292ca53f9d9a33e7188ad5
DeathRight <only.deathright@gmail.com>
2022-02-19 23:07:34 -0600

    feat: initial

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,60 +1,25 @@
 import Card from "@app/components/common/Card";
 import Center from "@app/components/common/Center";
 import LoadingOverlay from "@app/components/LoadingOverlay";
-import {
-  doc,
-  enableIndexedDbPersistence,
-  initializeFirestore,
-} from "firebase/firestore";
 import { FunctionComponent } from "react";
 import { Link } from "react-router-dom";
-import {
-  FirestoreProvider,
-  SuspenseWithPerf,
-  useFirestore,
-  useFirestoreDocDataOnce,
-  useInitFirestore,
-} from "reactfire";
+import { SuspenseWithPerf } from "reactfire";
+import { useAxios } from "use-axios";
 
-const FirestoreWrapper: FunctionComponent = ({ children }) => {
-  const { data: firestoreInstance } = useInitFirestore(async (firebaseApp) => {
-    const db = initializeFirestore(firebaseApp, {});
-    await enableIndexedDbPersistence(db);
-    return db;
-  });
-
-  return (
-    <FirestoreProvider sdk={firestoreInstance}>{children}</FirestoreProvider>
-  );
-};
-
-const StaticValue = () => {
-  const firestore = useFirestore();
-  const ref = doc(firestore, "pages/about");
-  const { data } = useFirestoreDocDataOnce(ref);
-
-  let { body } = data as { body: string };
-  body = body.split("\\n").join("\n");
-
-  return <div style={{ whiteSpace: "pre-line" }}>{body}</div>;
+const FetchHello = () => {
+  const hello = useAxios("/api/hello");
+  return <>{hello}</>;
 };
 
 const About = () => (
-  <SuspenseWithPerf fallback={<LoadingOverlay />} traceId="firestore-demo">
-    <FirestoreWrapper>
-      <Center>
-        <Card>
-          <SuspenseWithPerf
-            fallback="connecting to Firestore..."
-            traceId="firestore-demo-doc"
-          >
-            <StaticValue />
-            <hr />
-            <Link to="/">Home</Link>
-          </SuspenseWithPerf>
-        </Card>
-      </Center>
-    </FirestoreWrapper>
-  </SuspenseWithPerf>
+  <Center>
+    <Card>
+      <SuspenseWithPerf fallback={<LoadingOverlay />} traceId="hello-test">
+        <FetchHello />
+        <hr />
+        <Link to="/">Home</Link>
+      </SuspenseWithPerf>
+    </Card>
+  </Center>
 );
 export default About;
